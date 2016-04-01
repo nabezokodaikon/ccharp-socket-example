5d16f75a10627a9f3dffe05033d7fa9e48e4d02d
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-01 17:27:37 +0900

    add プロジェクトのテンプレートをVS2015で作成

---
+++ SocketExample/SocketServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using SocketServer.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SocketServer
+{
+    static class Program
+    {
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Thread.CurrentThread.Name = "Main";
+            log.Info("################################");
+            log.Debug("アプリケーションを開始します。");
+
+            Application.EnableVisualStyles();
+            Application.Run(new MainForm());
+
+            log.Debug("アプリケーションを終了します。");
+        }
+    }
+}
