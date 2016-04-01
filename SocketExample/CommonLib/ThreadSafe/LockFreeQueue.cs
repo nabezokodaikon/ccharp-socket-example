24a4e502cbab2ff563fa568b953aca3e513884ed
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-01 22:07:21 +0900

    add スレッドセーフな機能を持つクラスを実装
    参考: http://www.boyet.com/Articles/LockfreeQueue.html

---
+++ SocketExample/CommonLib/ThreadSafe/LockFreeQueue.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+// 参考: http://www.boyet.com/Articles/LockfreeQueue.html
+namespace CommonLib.ThreadSafe
+{
+    internal static class SyncMethods
+    {
+        public static bool TryUpdate<T>(ref T location, T comparand, T newValue)
+            where T : class
+        {
+            return comparand == Interlocked.CompareExchange(ref location, newValue, comparand);
+        }
+
+        public static bool IsMatch<T>(ref T location, T newValue)
+            where T : class
+        {
+            return newValue == Interlocked.CompareExchange(ref location, newValue, newValue);
+        }
+
+        public static bool IsNull<T>(ref T location)
+            where T : class
+        {
+            return null == Interlocked.CompareExchange(ref location, null, null);
+        }
+    }
+
+    internal class SingleLinkNode<T>
+    {
+        public SingleLinkNode<T> Next;
+        public T Item;
+    }
+
+    public class LockFreeQueue<T>
+    {
+        SingleLinkNode<T> head;
+        SingleLinkNode<T> tail;
+
+        public LockFreeQueue()
+        {
+            this.head = new SingleLinkNode<T>();
+            this.tail = this.head;
+        }
+
+        public void Enqueue(T item)
+        {
+            SingleLinkNode<T> oldTail = null;
+            SingleLinkNode<T> oldNext = null;
+            var node = new SingleLinkNode<T>();
+            node.Item = item;
+
+            var updatedNewLink = false;
+            while (!updatedNewLink)
+            {
+                oldTail = this.tail;
+                oldNext = oldTail.Next;
+                if (SyncMethods.IsMatch(ref this.tail, oldTail))
+                {
+                    if (SyncMethods.IsNull(ref oldNext))
+                    {
+                        updatedNewLink = SyncMethods.TryUpdate(ref this.tail.Next, null, node);
+                    }
+                    else
+                    {
+                        SyncMethods.TryUpdate(ref this.tail, oldTail, oldNext);
+                    }
+                }
+            }
+
+            SyncMethods.TryUpdate(ref this.tail, oldTail, node);
+        }
+
+        public T Dequeue()
+        {
+            T result = default(T);
+            var haveAdvancedHead = false;
+            while (!haveAdvancedHead)
+            {
+                var oldHead = this.head;
+                var oldTail = this.tail;
+                var oldHeadNext = oldHead.Next;
+
+                if (SyncMethods.IsMatch(ref oldHead, this.head))
+                {
+                    if (SyncMethods.IsMatch(ref oldHead, oldTail))
+                    {
+                        if (SyncMethods.IsNull(ref oldHeadNext))
+                        {
+                            return default(T);
+                        }
+
+                        SyncMethods.TryUpdate(ref this.tail, oldTail, oldHeadNext);
+                    }
+                    else
+                    {
+                        result = oldHeadNext.Item;
+                        haveAdvancedHead = SyncMethods.TryUpdate(ref this.head, oldHead, oldHeadNext);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void Clear()
+        {
+            while (this.Dequeue() != null) { }
+        }
+    }
+}
---
+++ SocketExample/CommonLib/ThreadSafe/ThreadSafeBoolean.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading;
+
+namespace CommonLib.ThreadSafe
+{
+    /// <summary>
+    /// スレッドセーフにアクセス可能な bool 値を保持するクラス。
+    /// </summary>
+    public sealed class ThreadSafeBoolean
+    {
+        private const long FALSE_VALUE = 0L;
+        private const long TRUE_VALUE = 1L;
+
+        private long value;
+
+        /// <summary>
+        /// 保持している値。
+        /// </summary>
+        public bool Value
+        {
+            get
+            {
+                return (Interlocked.Read(ref this.value) == TRUE_VALUE);
+            }
+            set
+            {
+                if (value)
+                {
+                    Interlocked.Exchange(ref this.value, TRUE_VALUE);
+                }
+                else
+                {
+                    Interlocked.Exchange(ref this.value, FALSE_VALUE);
+                }
+            }
+        }
+
+        /// <summary>
+        /// ThreadSafeBoolean クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="initValue">初期値。</param>
+        public ThreadSafeBoolean(bool initValue)
+        {
+            if (initValue)
+            {
+                this.value = TRUE_VALUE;
+            }
+            else
+            {
+                this.value = FALSE_VALUE;
+            }
+        }
+
+        /// <summary>
+        /// 値の変更を試みます。
+        /// </summary>
+        /// <param name="newValue">設定する値。</param>
+        /// <returns>値が変更できた場合はtrue。それ以外の場合はfalseを返します。</returns>
+        public bool TryChange(bool newValue)
+        {
+            if (newValue)
+            {
+                return (Interlocked.CompareExchange(ref this.value, TRUE_VALUE, FALSE_VALUE) == FALSE_VALUE);
+            }
+            else
+            {
+                return (Interlocked.CompareExchange(ref this.value, FALSE_VALUE, TRUE_VALUE) == TRUE_VALUE);
+            }
+        }
+    }
+}
